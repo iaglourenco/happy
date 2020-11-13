14b0311d7703838f03de4997d7aaea3f67c749b1
Iago <iagojlourenco@gmail.com>
2020-11-13 13:34:47 -0300

    page-deploy

---
+++ precache-manifest.c8e85f77eaf0734b3f0922fe2f65472c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b1fdf66af74683587e86ce4ed05ac9",
+    "url": "/happy/index.html"
+  },
+  {
+    "revision": "b624d11629a2010b8467",
+    "url": "/happy/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "9552b651454eea0bde54",
+    "url": "/happy/static/css/main.406fd3cb.chunk.css"
+  },
+  {
+    "revision": "b624d11629a2010b8467",
+    "url": "/happy/static/js/2.fead575c.chunk.js"
+  },
+  {
+    "revision": "3c56d32f1a0e992453f57615992b86b8",
+    "url": "/happy/static/js/2.fead575c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9552b651454eea0bde54",
+    "url": "/happy/static/js/main.a1943014.chunk.js"
+  },
+  {
+    "revision": "beffc59a84efbb1596c3",
+    "url": "/happy/static/js/runtime-main.61df2244.js"
+  },
+  {
+    "revision": "91c8d1af04a8fbdde51d2b16c5471b15",
+    "url": "/happy/static/media/landing.91c8d1af.svg"
+  },
+  {
+    "revision": "1653c64706c95e2e85a07cc755abf8ba",
+    "url": "/happy/static/media/logo.1653c647.svg"
+  },
+  {
+    "revision": "284d1ea859a0eb7d0ed785dbcc775752",
+    "url": "/happy/static/media/map-marker.284d1ea8.svg"
+  }
+]);
